b91bdb95f98ee3af9c6e386ba238b292cf759a41
Peter Jankuliak <p.jankuliak@gmail.com>
2018-08-31 15:33:13 +0200

    Change fennec's app title from "Fennec USER" to "CENO"

--- mobile/android/branding/unofficial/configure.sh
+++ mobile/android/branding/unofficial/configure.sh
@@ -2,8 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ANDROID_PACKAGE_NAME=org.mozilla.fennec_`echo ${USER:-unknown} | sed 's/-/_/g'`
-MOZ_APP_DISPLAYNAME="Fennec `echo ${USER:-unknown} | sed 's/-/_/g'`"
+#ANDROID_PACKAGE_NAME=org.mozilla.fennec_`echo ${USER:-unknown} | sed 's/-/_/g'`
+#MOZ_APP_DISPLAYNAME="Fennec `echo ${USER:-unknown} | sed 's/-/_/g'`"
+ANDROID_PACKAGE_NAME=org.mozilla.fennec_ceno
+MOZ_APP_DISPLAYNAME="CENO"
 MOZ_UPDATER=
 MOZ_ANDROID_ANR_REPORTER=
 MOZ_ANDROID_GCM_SENDERID=965234145045
